b32eae6cd13a97c59e3b13097249aed83b4d69f3
Mikołaj Pęcak <mikolajpecak@gmail.com>
2025-05-02 09:46:31 +0200

    Remove unnecessary files

---
+++ app/edit/page.tsx
@@ -0,0 +1,11 @@
+export default function Edit() {
+  return (
+    <main>
+      <h1>Edit message of the day</h1>
+      <form>
+        <input type="text" />
+        <button>Save</button>
+      </form>
+    </main>
+  );
+}
